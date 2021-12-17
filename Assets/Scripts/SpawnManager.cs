19f405dff4a2b93e28727854a00b522cfb3ee250
Kaan Akel <kaanakel@msn.com>
2021-12-17 17:45:58 +0300

    0.7v

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] balloonPrefabs;
-    public float spawnPosX = 20.0f;
-    public float spawnPosZ = 20.0f;
+    public float spawnPosX = 9.50f;
+    public float spawnPosZ = 9.50f;
+    public float spawnPosY = 5.0f;
     public float startDelay = 2.0f;
     public float startInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBalloon", startDelay, Random.Range(3f, 5f));
+        InvokeRepeating("BalloonSpawnChance", startDelay, Random.Range(3f, 5f));
     }
 
     // Update is called once per frame
@@ -22,11 +23,41 @@ public class SpawnManager : MonoBehaviour
 
     }
 
-    void SpawnRandomBalloon()
+    /*void SpawnRandomBalloon()
     {
         int balloonIndex = Random.Range(0, balloonPrefabs.Length);
-        Vector3 spawnPos = new Vector3(Random.Range(spawnPosX, -spawnPosX), 0, spawnPosZ);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnPosX, -spawnPosX), spawnPosY, spawnPosZ);
 
         Instantiate(balloonPrefabs[balloonIndex], spawnPos, balloonPrefabs[balloonIndex].transform.rotation);
+    }*/
+
+    public void BalloonSpawnChance()
+    {
+        if (Random.value > 0.5) //%50 percent chance
+        {//code here
+            int balloonIndex = Random.Range(0, balloonPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(spawnPosX, -spawnPosX), spawnPosY, spawnPosZ);
+
+            Instantiate(balloonPrefabs[0], spawnPos, balloonPrefabs[balloonIndex].transform.rotation);
+            return;
+        }
+
+        if (Random.value > 0.2) //%80 percent chance (1 - 0.2 is 0.8)
+        { //code here
+            int balloonIndex = Random.Range(0, balloonPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(spawnPosX, -spawnPosX), spawnPosY, spawnPosZ);
+
+            Instantiate(balloonPrefabs[1], spawnPos, balloonPrefabs[balloonIndex].transform.rotation);
+            return;
+        }
+
+        if (Random.value > 0.7) //%30 percent chance (1 - 0.7 is 0.3)
+        { //code here
+            int balloonIndex = Random.Range(0, balloonPrefabs.Length);
+            Vector3 spawnPos = new Vector3(Random.Range(spawnPosX, -spawnPosX), spawnPosY, spawnPosZ);
+
+            Instantiate(balloonPrefabs[2], spawnPos, balloonPrefabs[balloonIndex].transform.rotation);
+            return;
+        }
     }
 }
